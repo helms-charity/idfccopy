8b4a866016dbc4c3722fbabe0cf270ee7e97c897
Josh Bunting <bunting@adobe.com>
2025-12-11 15:26:31 -0700

    add Overview Block foundation

---
+++ blocks/overview/overview.js
@@ -0,0 +1,34 @@
+/* eslint-disable */
+
+/* placeholder-script */
+$(document).ready(function () {
+  var length = $("#gcoverview").data("words") || 150;
+  cHtml = sanitize($("#gcoverview").html());
+  cText = sanitize($("#gcoverview").text().substr(0, length).trim());
+  $("#gcoverview")
+    .addClass("compressed")
+    .html(cText + "... <a href='#' class='exp'>Read More</a>");
+  window.handler = function () {
+    $(".exp").click(function () {
+      if ($("#gcoverview").hasClass("compressed")) {
+        $("#gcoverview").html(
+          cHtml + "<a href='#' class='exp'>Read Less</a>"
+        );
+        $("#gcoverview").removeClass("compressed");
+        handler();
+        return false;
+      } else {
+        $("#gcoverview").html(
+          cText + "... <a href='#' class='exp'>Read More</a>"
+        );
+        $("#gcoverview").addClass("compressed");
+        handler();
+        return false;
+      }
+    });
+  };
+  handler();
+
+  
+ 
+});
