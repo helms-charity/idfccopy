50949a8d0763aa9c2e33e94634aacc7769dfb3e5
Charity Helms <chelms@adobe.com>
2026-01-13 13:22:59 -0500

    new json and js

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -17,40 +17,18 @@ export default function decorate(block) {
     if (headingText) headingText.classList.add('banner-heading-text');
   }
 
-  // CTA 1
-  if (children[1]) {
-    children[1].classList.add('banner-cta-1', 'banner-cta');
-    const ctaHeading = children[1].querySelector('p:first-of-type');
-    const ctaLink = children[1].querySelector('p:last-of-type a');
-    const ctaHr = children[1].querySelectorAll('hr');
-    if (ctaHeading) ctaHeading.classList.add('cta-heading');
-    if (ctaLink) ctaLink.classList.add('cta-button', 'cta-button-outlined');
-    ctaHr.forEach((hr) => hr.classList.add('cta-separator'));
-  }
-
-  // CTA 2
-  if (children[2]) {
-    children[2].classList.add('banner-cta-2', 'banner-cta');
-    const ctaHeading = children[2].querySelector('p:first-of-type');
-    const ctaLink = children[2].querySelector('p:last-of-type a');
-    const ctaHr = children[2].querySelectorAll('hr');
-    if (ctaHeading) ctaHeading.classList.add('cta-heading');
-    if (ctaLink) ctaLink.classList.add('cta-button', 'cta-button-filled');
-    ctaHr.forEach((hr) => hr.classList.add('cta-separator'));
-  }
-
   // Bottom Text
-  if (children[3]) {
-    children[3].classList.add('banner-bottom-text');
-    const bottomText = children[3].querySelector('p');
+  if (children[2]) {
+    children[2].classList.add('banner-bottom-text');
+    const bottomText = children[2].querySelector('p');
     if (bottomText) bottomText.classList.add('banner-bottom-text-content');
   }
 
   // Desktop Image
-  if (children[4]) {
-    children[4].classList.add('banner-image-desktop', 'banner-image');
-    const picture = children[4].querySelector('picture');
-    const img = children[4].querySelector('img');
+  if (children[3]) {
+    children[3].classList.add('banner-image-desktop', 'banner-image');
+    const picture = children[3].querySelector('picture');
+    const img = children[3].querySelector('img');
     if (picture) picture.classList.add('banner-picture');
     if (img) {
       img.classList.add('banner-img');
@@ -59,10 +37,10 @@ export default function decorate(block) {
   }
 
   // Mobile Image
-  if (children[5]) {
-    children[5].classList.add('banner-image-mobile', 'banner-image');
-    const picture = children[5].querySelector('picture');
-    const img = children[5].querySelector('img');
+  if (children[4]) {
+    children[4].classList.add('banner-image-mobile', 'banner-image');
+    const picture = children[4].querySelector('picture');
+    const img = children[4].querySelector('img');
     if (picture) picture.classList.add('banner-picture');
     if (img) {
       img.classList.add('banner-img');
