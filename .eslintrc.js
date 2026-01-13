09fdf3d0f71a660dc67328b02a55382c15905a8c
Charity Helms <chelms@adobe.com>
2026-01-13 07:23:20 -0500

    increase section allowed cells

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,7 @@ module.exports = {
     'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
     'no-param-reassign': [2, { props: false }], // allow modifying properties of param
     'xwalk/max-cells': ['error', {
-      section: 20, // section is a key-value block and over 4 is OK
+      section: 30, // section is a key-value block and over 4 is OK
       accordion: 5, // the rest of these need adjustments per https://www.aem.live/developer/component-model-definitions#type-inference
       'banner-item': 6,
       cards: 8,
