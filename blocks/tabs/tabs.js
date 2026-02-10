0fbf9fe6742a80790534f6ae701d9c23e9a12e78
Charity Helms <chelms@adobe.com>
2026-02-10 09:11:58 -0500

    move the moveInstrumentation before tabTextWrapper test

--- blocks/tabs/tabs.js
+++ blocks/tabs/tabs.js
@@ -63,6 +63,7 @@ export default async function decorate(block) {
       tabpanel.setAttribute('aria-hidden', !!i);
       tabpanel.setAttribute('aria-labelledby', `tab-${id}`);
       tabpanel.setAttribute('role', 'tabpanel');
+      moveInstrumentation(tab.parentElement, tabpanel.lastElementChild);
 
       const tabTextWrapper = document.createElement('div');
       if (tabName) {
@@ -101,7 +102,6 @@ export default async function decorate(block) {
       tabpanel.setAttribute('aria-labelledby', `tab-${id}`);
       tabpanel.setAttribute('role', 'tabpanel');
 
-   //   moveInstrumentation(tab.parentElement, tabpanel.lastElementChild);
       const button = createTabButton(
         block,
         tabpanel,
